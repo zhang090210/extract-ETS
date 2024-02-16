40f1abeeccca2d75387acb6e7f007eb6e3d04e61
Zzy <626106510@qq.com>
2024-02-16 17:41:16 +0800

    0.0.1版本编写完毕

---
+++ generate.py
@@ -0,0 +1,76 @@
+from typing import Union
+
+import dominate
+from dominate.tags import *
+import pdfkit
+
+from get_data import *
+
+
+def to_html(aws: list, save_path: Union[None, str] = None):
+    chooses = []
+    roles = []
+    pictures = []
+    reads = []
+    for answer in aws:
+        match answer:
+            case Choose():
+                chooses.append(answer)
+            case Role():
+                roles.append(answer)
+            case Picture():
+                pictures.append(answer)
+            case Read():
+                reads.append(answer)
+
+    doc = dominate.document(title="result")
+    with doc.head:
+        meta(charset="utf-8")
+    with doc.body:
+        h1("听后选择")
+        for choose in chooses:
+            h2("原文：")
+            for textual in choose.textual:
+                p(textual)
+            for c in choose.questions:
+                h3(c['stem'])
+                for option in c['options']:
+                    p(option)
+                h4(f"Answer: {c['answer']}")
+                br()
+            br()
+        h1("听后回答")
+        for role in roles:
+            h2("原文")
+            for textual in role.textual:
+                p(textual)
+            for ro in role.questions:
+                h3(ro['stem'])
+                for answer in ro['answer']:
+                    p(answer)
+            br()
+        br()
+        h1("听后转述")
+        for picture in pictures:
+            for answer in picture.answers:
+                p(answer)
+                br()
+            br()
+        h1("朗读短文")
+        for read in reads:
+            p(read.textual)
+
+    if save_path is None:
+        return doc.render()
+    elif isinstance(save_path, str):
+        with open(save_path, "w", encoding="utf-8") as f:
+            f.write(doc.render())
+
+
+def to_pdf(aws: list, save_path="result.pdf"):
+    doc = to_html(aws)
+    # 将wkhtmltopdf.exe程序绝对路径传入config对象
+    path_wkthmltopdf = r'include\\wkhtmltox\\bin\\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
+    # 生成pdf文件，to_file为文件路径
+    pdfkit.from_string(doc, save_path, configuration=config, verbose=True)
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello")
+import os
+
+from generate import *
+
+if __name__ == '__main__':
+    # 获取所有题目答案文件
+    datas = []
+    for root, dirs, files in os.walk(".\\data", topdown=False):
+        for name in files:
+            if name == "content2.json":
+                datas.append(os.path.join(root, name))
+
+    answers = []
+    for data in datas:
+        with open(data, "r", encoding="utf-8") as f:
+            content = f.read()
+        ans = QuestionType.from_json(content)
+        answers.append(ans)
+
+    to_html(answers, "result.html")
+    to_pdf(answers)
